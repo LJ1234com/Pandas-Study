1ebce095a4416cb27a1916b81155d5c1ca4138ab
LJ1234com <46641321+LJ1234com@users.noreply.github.com>
2019-01-14 22:11:16 +0800

    Update 10-Working_with_Text_Data.py

--- 10-Working_with_Text_Data.py
+++ 10-Working_with_Text_Data.py
@@ -1,46 +1,46 @@
-import pandas as pd       
-import numpy as np        
-                          
-'''                       
- lower()	            Co
- upper()	            Co
- len()	                Co
- strip()	            He
- split(' ')	            Sp
- cat(sep=' ')	        Co
- get_dummies()	        Re
- contains(pattern)	    Re
- replace(a,b)	        Re
- repeat(value)	        Re
- count(pattern)	        Re
- startswith(pattern)	Re
- endswith(pattern)	    Re
- find(pattern)	        Re
- findall(pattern)	    Re
- swapcase	            Sw
- islower()	            Ch
- isupper()             	Ch
- isnumeric()	        Ch
-'''                       
-                          
-s = pd.Series(['Tom', 'Wil
-print(s)                  
-print(s.str.lower())      
-print(s.str.upper())      
-print(s.str.len())        
-print(s.str.strip())      
-print(s.str.split(' '))   
-print(s.str.cat(sep='_')) 
+import pandas as pd
+import numpy as np
+
+'''
+ lower()	            Converts strings in the Series/Index to lower case.
+ upper()	            Converts strings in the Series/Index to upper case.
+ len()	                Computes String length().
+ strip()	            Helps strip whitespace(including newline) from each string in the Series/index from both the sides.
+ split(' ')	            Splits each string with the given pattern.
+ cat(sep=' ')	        Concatenates the series/index elements with given separator.
+ get_dummies()	        Returns the DataFrame with One-Hot Encoded values.
+ contains(pattern)	    Returns a Boolean value True for each element if the substring contains in the element, else False.
+ replace(a,b)	        Replaces the value a with the value b.
+ repeat(value)	        Repeats each element with specified number of times.
+ count(pattern)	        Returns count of appearance of pattern in each element.
+ startswith(pattern)	Returns true if the element in the Series/Index starts with the pattern.
+ endswith(pattern)	    Returns true if the element in the Series/Index ends with the pattern.
+ find(pattern)	        Returns the first position of the first occurrence of the pattern.
+ findall(pattern)	    Returns a list of all occurrence of the pattern.
+ swapcase	            Swaps the case lower/upper.
+ islower()	            Checks whether all characters in each string in the Series/Index in lower case or not. Returns Boolean
+ isupper()             	Checks whether all characters in each string in the Series/Index in upper case or not. Returns Boolean.
+ isnumeric()	        Checks whether all characters in each string in the Series/Index are numeric. Returns Boolean.
+'''
+
+s = pd.Series(['Tom', 'William Rick', 'John', 'Alber@t', np.nan, '1234','SteveSmith'])
+print(s)
+print(s.str.lower())
+print(s.str.upper())
+print(s.str.len())
+print(s.str.strip())
+print(s.str.split(' '))
+print(s.str.cat(sep='_'))
 print(s.str.get_dummies())
 print(s.str.contains(' '))
-print(s.str.replace('@', '
-print(s.str.repeat(2))    
-print(s.str.count('m'))   
-print(s.str.startswith('T'
+print(s.str.replace('@', '$'))
+print(s.str.repeat(2))
+print(s.str.count('m'))
+print(s.str.startswith('T'))
 print(s.str.endswith('t'))
-print(s.str.find('e'))    
-print(s.str.findall('e')) 
-print(s.str.swapcase())   
-print(s.str.islower())    
-print(s.str.isupper())    
-print(s.str.isnumeric())  
+print(s.str.find('e'))
+print(s.str.findall('e'))
+print(s.str.swapcase())
+print(s.str.islower())
+print(s.str.isupper())
+print(s.str.isnumeric())
