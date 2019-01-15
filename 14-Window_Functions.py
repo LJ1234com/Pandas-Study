a28be797fec25c9eaecb147b74f258f79da6146a
LJ1234com <46641321+LJ1234com@users.noreply.github.com>
2019-01-15 12:48:04 +0800

    Update 14-Window_Functions.py

--- 14-Window_Functions.py
+++ 14-Window_Functions.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import numpy as np
 
+'''
+ .rolling() 
+ .expanding()
+ .ewm()
+'''
+
+df = pd.DataFrame(np.random.randn(10, 4),
+index = pd.date_range('1/1/2000', periods=10),
+columns = ['A', 'B', 'C', 'D'])
+
+## rolling()
+print(df)
+print(df.rolling(window=2).mean())
+print(df.rolling(window=2).sum())
+
+
+## expanding()
+print(df.expanding(min_periods=2).mean())
+print(df.expanding(min_periods=2).sum())
+
+## ewm()
+print(df.ewm(com=0.5).mean())
